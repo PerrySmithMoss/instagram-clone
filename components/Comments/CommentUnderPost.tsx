4179a025942797a7e0713757152adb9c4a0a19f6
Perry Smith-Moss <perrysmithmoss@gmail.com>
2022-10-02 16:45:58 +0100

    Users can now like comments directly under a post

---
+++ components/Comments/CommentUnderPost.tsx
@@ -0,0 +1,111 @@
+import {
+  deleteDoc,
+  doc,
+  setDoc,
+  onSnapshot,
+  query,
+  collection,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { db } from "../../firebase";
+import styles from "./Comments.module.css";
+
+interface ICommentUnderPostProps {
+  comment: any;
+  postId: string;
+}
+
+export const CommentUnderPost: React.FC<ICommentUnderPostProps> = ({
+  comment,
+  postId,
+}) => {
+  const { user } = useAuth();
+  const [likes, setLikes] = useState<any[]>([]);
+  const [hasLikedComment, setHasLikedComment] = useState(false);
+
+  async function handleLikeComment() {
+    if (hasLikedComment) {
+      await deleteDoc(
+        doc(db, "posts", postId, "comments", comment.id, "likes", user.uid)
+      );
+    } else {
+      await setDoc(
+        doc(db, "posts", postId, "comments", comment.id, "likes", user.uid),
+        {
+          username: user?.username,
+          userAvatar: user?.photoUrl,
+        }
+      );
+    }
+  }
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      query(collection(db, `/posts/${postId}/comments/${comment.id}/likes`)),
+      (querySnapshot) => {
+        const documents = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          };
+        });
+        setLikes(documents);
+      }
+    );
+
+    return () => unSub();
+  }, [db, comment.id, postId]);
+
+  useEffect(
+    () =>
+      setHasLikedComment(
+        likes.findIndex((like: any) => like.id === user?.uid) !== -1
+      ),
+    [likes]
+  );
+  return (
+    <div className="flex justify-between items-center space-x-1 mt-0.5">
+      <div>
+        <p>
+          <span className="text-sm font-bold">{comment.username}</span>
+          <span className="m-0 inline">&nbsp;</span>
+          <span className="text-sm">{comment.comment}</span>
+        </p>
+      </div>
+      {hasLikedComment ? (
+        <div
+          onClick={handleLikeComment}
+          className={`${styles.likeButton} like cursor-pointer`}
+        >
+          <svg
+            aria-label="Unlike"
+            color="#ed4956"
+            fill="#ed4956"
+            height="12"
+            role="img"
+            viewBox="0 0 48 48"
+            width="12"
+          >
+            <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+          </svg>
+        </div>
+      ) : (
+        <div
+          onClick={handleLikeComment}
+          className={`${styles.likeButton} like cursor-pointer`}
+        >
+          <svg
+            aria-label="Like"
+            fill="#262626"
+            height="12"
+            viewBox="0 0 48 48"
+            width="12"
+          >
+            <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+          </svg>
+        </div>
+      )}
+    </div>
+  );
+};
--- components/Posts/Post.tsx
+++ components/Posts/Post.tsx
@@ -15,6 +15,7 @@ import { useAuth } from "../../context/AuthContext";
 import { db } from "../../firebase";
 import { Post as IPost } from "../../types/post";
 import { Comment } from "../Comments/Comment";
+import { CommentUnderPost } from "../Comments/CommentUnderPost";
 import { ViewPostModal } from "../Modal/ViewPost/ViewPostModal";
 import styles from "./Posts.module.css";
 
@@ -100,7 +101,6 @@ export const Post: React.FC<IPostProps> = ({ post }) => {
   }
 
   async function handleLikeComment(commentId: string) {
-
     // if (hasLikedComment) {
     //   await deleteDoc(doc(db, "posts", post.id, "comments", user.uid));
     // } else {
@@ -327,68 +327,16 @@ export const Post: React.FC<IPostProps> = ({ post }) => {
                   .slice(0, 2)
                   .reverse()
                   .map((comment: any) => (
-                    <div
-                      key={comment.id}
-                      className="flex justify-between items-center space-x-1 mt-0.5"
-                    >
-                      <div>
-                        <p>
-                          <span className="text-sm font-bold">
-                            {comment.username}
-                          </span>
-                          <span className="m-0 inline">&nbsp;</span>
-                          <span className="text-sm">{comment.comment}</span>
-                        </p>
-                      </div>
-                      <div className={styles.likeButtonContainer}>
-                        <svg
-                          aria-label="Like"
-                          color="#262626"
-                          fill="#262626"
-                          height="12"
-                          role="img"
-                          viewBox="0 0 24 24"
-                          width="12"
-                        >
-                          <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
-                        </svg>
-                      </div>
-                    </div>
+                    <CommentUnderPost key={comment.id} comment={comment} postId={post.id} />
                   ))}
               </div>
             ) : (
-              <div className="mt-">
+              <div className="mt-1">
                 {comments
                   .slice(0, 2)
                   .reverse()
                   .map((comment: any) => (
-                    <div
-                      key={comment.id}
-                      className="flex justify-between items-center space-x-1"
-                    >
-                      <div>
-                        <p>
-                          <span className="text-sm font-bold">
-                            {comment.username}
-                          </span>
-                          <span className="m-0 inline">&nbsp;</span>
-                          <span className="text-sm">{comment.comment}</span>
-                        </p>
-                      </div>
-                      <div className={styles.likeButtonContainer}>
-                        <svg
-                          aria-label="Like"
-                          color="#262626"
-                          fill="#262626"
-                          height="12"
-                          role="img"
-                          viewBox="0 0 24 24"
-                          width="12"
-                        >
-                          <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
-                        </svg>
-                      </div>
-                    </div>
+                    <CommentUnderPost key={comment.id} comment={comment} postId={post.id} />
                   ))}
               </div>
             )}
@@ -766,7 +714,7 @@ export const Post: React.FC<IPostProps> = ({ post }) => {
                                 tabIndex={0}
                                 className="flex flex-col items-stretch flex-shrink-0 m-0 p-0 relative"
                               >
-                                  <Comment postId={post.id} comment={comment} />
+                                <Comment postId={post.id} comment={comment} />
                               </div>
                             </ul>
                           ))}
